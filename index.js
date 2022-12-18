51d4a9419ea50e535b263dfa16cbeb2611719bc9
Phammerman <103281235+Phammerman@users.noreply.github.com>
2022-12-18 14:21:40 +0100

    Setup
    HTML layout setup and basic css style applied.
    
    TODO's added in js file

---
+++ index.js
@@ -0,0 +1,15 @@
+/* TODO next
+
+JS
+
+- set password-el as a variable
+- set the password characters as an Array
+- add eventlistener to button
+
+CSS
+
+- center everthing
+- center and align password el between each other
+- hover effect on button
+
+*/
